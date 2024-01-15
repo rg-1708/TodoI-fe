649ac4f49def54004d342d58f49103fc203b5591
Ramil Gojayev <gojayram@fit.cvut.cz>
2024-01-15 03:09:57 +0100

    Initial RTK Setup

---
+++ src/redux/todo-slice.ts
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Todo } from "../types/todo.interface";
+
+export interface TodoState {
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  todos: [
+    {
+      id: 1,
+      title: "todo1",
+      description: "do a barrel roll",
+      status: "Completed",
+    },
+    {
+      id: 2,
+      title: "todo2",
+      description: "do a backflip",
+      status: "Pending",
+    },
+    {
+      id: 3,
+      title: "todo3",
+      description: "play bohemian rhapsody",
+      status: "InProgress",
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        description: action.payload.description,
+        status: "Pending",
+      } as Todo;
+
+      state.todos.push(newTodo);
+    },
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
